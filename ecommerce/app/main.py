2c0126fbb95900a5526535a15627296bc281d827
Jeevant Singh <anupamasinghsrinet1976@gmail.com>
2026-01-01 00:18:16 +0530

    product_loader

--- ecommerce/app/main.py
+++ ecommerce/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
 from .services.products import get_all_products
 
 app = FastAPI()
@@ -8,6 +8,15 @@ def root():
     return {"message" : "welcome"}
 
 @app.get("/products")
-def get_products():
-    return get_all_products()
+def list_product(name: str = Query(default=None, min_length=1, max_length=50, description="Search by product name")):
+    products = get_all_products()
+    if name :
+        searched_query = name.strip().lower()
+        products = [p for p in products if searched_query in p.get("name" , "").lower()]
 
+    if not products:
+        raise HTTPException( status_code = 404 , detail=f"No product found matching name ={name}")
+        
+    total = len(products)
+
+    return { "total" : total, "items" : products }
